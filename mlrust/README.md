0e701a0a687270f45c44e35135f515d779612161
mwrites <20499416+mwrites@users.noreply.github.com>
2024-08-13 20:54:02 +0200

    some caching for rust img

--- mlrust/README.md
+++ mlrust/README.md
@@ -2,7 +2,10 @@
 
 Build and push the image to docker hub
 ```bash
-make run TAG=latest
+make run TAG=v1.0.0
 ```
 
-
+ssh into container running your image
+```bash
+cargo run --example mixtral --release  -- --prompt "def print_prime(n): "
+```
--- mlrust/start-ssh-only.sh
+++ mlrust/start-ssh-only.sh
@@ -2,6 +2,23 @@
 
 echo "pod started"
 
+# Set environment variables for Rust, CUDNN, and CUDA
+export PATH="/usr/local/cuda/bin:/usr/local/cargo/bin:${PATH}"
+export RUSTUP_HOME="/usr/local/rustup"
+export CARGO_HOME="/usr/local/cargo"
+export RUSTFLAGS="-C link-arg=-fuse-ld=lld"
+export CUDNN_PATH="/usr/lib/x86_64-linux-gnu/libcudnn.so"
+export LD_LIBRARY_PATH="/usr/local/cuda/lib64:${LD_LIBRARY_PATH}"
+
+# Verify environment variables
+echo "Verifying environment variables:"
+echo "PATH: $PATH"
+echo "RUSTUP_HOME: $RUSTUP_HOME"
+echo "CARGO_HOME: $CARGO_HOME"
+echo "RUSTFLAGS: $RUSTFLAGS"
+echo "CUDNN_PATH: $CUDNN_PATH"
+echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
+
 if [[ $PUBLIC_KEY ]]
 then
     mkdir -p ~/.ssh
